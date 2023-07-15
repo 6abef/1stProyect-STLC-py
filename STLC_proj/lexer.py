d8c7b79b78fe4199492725c1144dc45cb8911a04
6abef <139399618+6abef@users.noreply.github.com>
2023-07-15 15:02:08 -0600

    Se agregó la configuración para test

--- STLC_proj/lexer.py
+++ STLC_proj/lexer.py
@@ -9,152 +9,196 @@ from dataclasses import dataclass
 from typing import Union, Optional
 
 
-Token = Union["Variable", "Int", "IntType", "Operator", "Bool", "BoolType", "LeftP", "RightP", "TokenError", "ArrowR", "LineLambda", "Twop", "UnitType", "UnitExp"]
-
+Token = Union[
+    "Variable",
+    "Int",
+    "IntType",
+    "Operator",
+    "Bool",
+    "BoolType",
+    "LeftP",
+    "RightP",
+    "TokenError",
+    "ArrowR",
+    "LineLambda",
+    "Twop",
+    "UnitType",
+    "UnitExp",
+]
 
 
 @dataclass
 class Int:
     value: int
 
+
 @dataclass
 class IntType:
     pass
 
-@dataclass    
+
+@dataclass
 class Operator:
     name: str
 
-@dataclass    
+
+@dataclass
 class Bool:
     value: bool
-    
-@dataclass   
+
+
+@dataclass
 class Variable:
     name: str
-    
-@dataclass    
+
+
+@dataclass
 class BoolType:
     pass
- 
+
+
 @dataclass
 class TokenError:
-    error: str 
+    error: str
+
 
 @dataclass
 class LeftP:
     pass
 
+
 @dataclass
 class RightP:
     pass
+
 
 @dataclass
 class ArrowR:
     pass
 
+
 @dataclass
 class LineLambda:
     pass
+
 
 @dataclass
 class Twop:
     pass
 
+
 @dataclass
 class UnitExp:
     pass
+
 
 @dataclass
 class UnitType:
     pass
 
+
 @dataclass
 class Stream:
-    value:str
+    value: str
     pos: int
-    def __init__(self,value:str):
+
+    def __init__(self, value: str):
         self.pos = 0
-        self.value=value
-        
-    def get_char(self)->Optional[str]:
-        if len(self.value)>self.pos:
+        self.value = value
+
+    def get_char(self) -> Optional[str]:
+        if len(self.value) > self.pos:
             return self.value[self.pos]
         else:
-            #Esta parte podria saltarse y dejar que solo retorne None
-            return None 
+            # Esta parte podria saltarse y dejar que solo retorne None
+            return None
+
     def consume(self):
-        self.pos+=1   
-        
+        self.pos += 1
+
     def get_posicion(self):
         return self.pos
-    
+
     def colcar_posicion(self, new_pos):
         self.pos = new_pos
-        
-def is_digit(string:str)->bool:
-    """ Esta funcion verifica el primer caracter de un string sea un dígito"""
-    if len(string)>0:
+
+
+def is_digit(string: str) -> bool:
+    """Esta funcion verifica el primer caracter de un string sea un dígito"""
+    if len(string) > 0:
         digit = string[0]
         if digit >= "0" and digit <= "9":
             return True
+        return False  # Se podría quitar y dejar que regrese False en cualquier caso. >Dejarlo da claridad
     else:
         return False
 
 
-
-def lexer_variable (stream:Stream)->Optional[Variable]:
-    acc:[str]=[]
-    orig_post=stream.get_posicion()
-    char=stream.get_char()
+def lexer_variable(stream: Stream) -> Optional[Variable]:
+    acc: list[str] = []
+    orig_post = stream.get_posicion()
+    char = stream.get_char()
     if char is None:
         return None
-    else: 
-        if (char=="_")or(char>="a" and char<="z")or(char>="A" and char<="Z"):
+    else:
+        if (
+            (char == "_")
+            or (char >= "a" and char <= "z")
+            or (char >= "A" and char <= "Z")
+        ):
             acc.append(char)
             stream.consume()
-            char=stream.get_char()
+            char = stream.get_char()
             if char is None:
-                if  (acc[0]>="a" and acc[0]<="z")or(acc[0]>="A" and acc[0]<="Z"):
+                if (acc[0] >= "a" and acc[0] <= "z") or (
+                    acc[0] >= "A" and acc[0] <= "Z"
+                ):
                     return Variable(acc[0])
                 stream.colcar_posicion(orig_post)
                 return None
-            while((char>="a" and char<="z")or(char>="A" and char<="Z")):
+            while (char >= "a" and char <= "z") or (
+                char >= "A" and char <= "Z"
+            ):
                 acc.append(char)
                 stream.consume()
-                char=stream.get_char()
+                char = stream.get_char()
                 if char is None:
                     break
-            
-            final_str= "".join(acc)
-            if final_str =="_":
+
+            final_str = "".join(acc)
+            if final_str == "_":
                 return None
             return Variable("".join(acc))
         else:
             return None
 
-print(lexer_variable(Stream("_augiugk$b")))    
+
+print(lexer_variable(Stream("_augiugk$b")))
 
 
-def lexer_int(stream:Stream)->Optional[Int]:
-    acc:[str]=[]
-    orig_post=stream.get_posicion()
+def lexer_int(stream: Stream) -> Optional[Int]:
+    acc: list[str] = []
+    orig_post = stream.get_posicion()
     num = stream.get_char()
     if num is None:
+        stream.colcar_posicion(orig_post)
         return None
-    else: 
+    else:
         if num == "-":
             acc.append(num)
             stream.consume()
             num = stream.get_char()
-            if is_digit(num) :
+            if num is None:
+                stream.colcar_posicion(orig_post)
+                return None
+            if is_digit(num):
                 while is_digit(num):
                     acc.append(num)
                     stream.consume()
                     num = stream.get_char()
                     if num is None:
                         break
-            else: 
+            else:
                 stream.colcar_posicion(orig_post)
                 return None
         elif is_digit(num):
@@ -168,8 +212,11 @@ def lexer_int(stream:Stream)->Optional[Int]:
         else:
             stream.colcar_posicion(orig_post)
             return None
-    return Int(int("".join(acc)))  
-            
+    return Int(int("".join(acc)))
+
+
+
+"""            
 print(lexer_int(Stream("-121+3-4"))) 
         
 b = Stream("_fgsfds-56365")
@@ -180,38 +227,4 @@ print(t,"t")
 p =lexer_variable(b)
 print(p,"p")
 print(b,"b")
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
