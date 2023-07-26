cd8d64ce4ac3cd5da2c0220d1c3adf857b04926c
6abef <139399618+6abef@users.noreply.github.com>
2023-07-25 23:01:57 -0600

    Funcion id_lexer y reader_lexer para refactorización y test bool

--- STLC_proj/lexer.py
+++ STLC_proj/lexer.py
@@ -6,7 +6,7 @@ Created on Mon Jul 10 12:10:34 2023
 Definición de clases
 """
 from dataclasses import dataclass
-from typing import Union, Optional
+from typing import Union, Optional, TypeVar, Callable
 
 
 Token = Union[
@@ -68,11 +68,6 @@ class UnitType:
 
 
 @dataclass
-class TokenError:
-    error: str
-
-
-@dataclass
 class LeftP:
     pass
 
@@ -94,6 +89,20 @@ class LineLambda:
 
 @dataclass
 class Twop:
+    pass
+
+
+@dataclass
+class If:
+    pass
+
+@dataclass
+class Then:
+    pass
+
+
+@dataclass
+class Equals:
     pass
 
 
@@ -123,6 +132,11 @@ class Stream:
         self.pos = new_pos
 
 
+@dataclass
+class TokenError:
+    error: str
+
+
 def is_digit(string: str) -> bool:
     """Esta funcion verifica el primer caracter de un string sea un dígito"""
     if len(string) > 0:
@@ -134,27 +148,74 @@ def is_digit(string: str) -> bool:
         return False
 
 
-def is_operator(string: str) -> bool:
-    # Verifica que sea operador
-    if len(string) > 0:
-        operator = string[0]
-        if (
-            operator == "+"
-            or operator == "-"
-            or operator == "*"
-            or operator == "/"
-            or operator == "<"
-            or operator == ">"
-            or operator == "="  # Para verificar una igualdad en bool
-            or operator == "&"
-            or operator == "|"
-            or operator == "~"
-        ):
-            return True
-    return False
+def id_lexer(
+    string: str,
+) -> Callable[
+    [Stream], Optional[str]
+]:  # buscador del lexer de una cadena reconocible
+    def lexer(stream: Stream) -> Optional[str]:
+        position = stream.get_posicion()
+        s = stream.value[position:]
+        # print("\tcadena a leer: ",s, "desde posición: ", position)
+        # print("\tsímbolo a comparar: ",string)
+        # Sprint("\tEvaluación: ",s.startswith(string))
+        if s.startswith(string):
+            # print("\tSi se encontró")
+            # print("\t__________________________________")
+            return string
+        else:
+            # print("\tNo se encontró")
+            # print("\t__________________________________")
+            return None
+
+    return lexer
 
 
-def lexer_variable(stream: Stream) -> Optional[Variable]:
+"""l = Stream("True")
+lexer_l = id_lexer("True")
+print(l.value, lexer_l(l))"""
+
+
+T = TypeVar("T")
+
+
+def reader_lexer(
+    symbol: str, varType: T
+) -> Callable[
+    [Stream], Optional[T]
+]:  # ejecutor de lexer para cadena identificada
+    lexer = id_lexer(
+        symbol
+    )  # Cuidado: En símbolos dobles (<=), reconocerá el símbolo unitario (<)
+
+    def evaluator(stream: Stream) -> Optional[T]:
+        evaluation = lexer(stream)
+        # print("Resultado de la evaluación de",stream.value,"vs",symbol," | ",evaluation)
+        if evaluation is None:
+            return None
+        else:
+            stream.colcar_posicion(
+                stream.get_posicion() + len(symbol)
+            )  # cambia la posición una vez que se identifica un tipo
+            # print("\tSe envía: ", varType)
+            return varType
+
+    return evaluator
+
+
+"""l = Stream("- 546")
+reader_l = reader_lexer("- ",Operator("- "))
+print(l.value, reader_l(l))
+
+l = Stream("<= 5")
+reader_l = reader_lexer("<=",Operator("<="))
+print(l.value, reader_l(l))
+"""
+
+
+def lexer_variable(
+    stream: Stream,
+) -> Optional[Variable]:  # buscador de variables
     acc: list[str] = []
     orig_post = stream.get_posicion()
     char = stream.get_char()
@@ -199,21 +260,10 @@ def lexer_variable(stream: Stream) -> Optional[Variable]:
             return None
 
 
-#print(lexer_variable(Stream("_augiugk$b")))
-print("Prueba lexer_variable: ")
-#s = Stream("_")
-#print(lexer_variable(s),s.get_posicion())
-
-#s = Stream("_variable")
-#lex_var =lexer_variable(s)
-#if lex_var is not None:
-#    print(len(lex_var.name),s.get_posicion())
-
-
-def lexer_int(stream: Stream) -> Optional[Int]:
+def lexer_int(stream: Stream) -> Optional[Int]:  # buscador de enteros
     acc: list[str] = []
     orig_post = stream.get_posicion()
-    num = stream.get_char()    
+    num = stream.get_char()
     if num is None:
         stream.colcar_posicion(orig_post)
         return None
@@ -248,142 +298,137 @@ def lexer_int(stream: Stream) -> Optional[Int]:
     return Int(int("".join(acc)))
 
 
-"""            
-print(lexer_int(Stream("-121+3-4"))) 
-        
-b = Stream("_fgsfds-56365")
-print(b,"b")
-t = lexer_int(b)
-print(b,"b")
-print(t,"t")
-p =lexer_variable(b)
-print(p,"p")
-print(b,"b")
-"""
+def lexer_operator(
+    stream: Stream,
+) -> Optional[Operator]:  # buscador de operadores
+    operator_symbols = [
+        "+",
+        "- ",
+        "*",
+        "/",
+        "<=",
+        ">=",
+        "==",
+        "<",
+        ">",
+        "&",
+        "|",
+        "~",
+    ]  # compara  <=, >= antes que < e > para evitar errores
+    for op in operator_symbols:
+        lexer = reader_lexer(op, Operator(op))
+        evaluation = lexer(stream)
+        # print(stream.value, "comparado con", Operator(op), "es:",comparation)
+        if evaluation is not None:
+            # print("Se acabó")
+            return evaluation
+    return None
 
 
-def lexer_operator(stream: Stream) -> Optional[Operator]:
-    orig_post = stream.get_posicion()
-    char = stream.get_char()
-    if char is None:
-        return None
-    if is_operator(char):
-        match char:
-            case "-":
-                stream.consume()
-                next_char = stream.get_char()
-                if next_char is None:
-                    return Operator(char)
-                if is_digit(next_char):
-                    stream.colcar_posicion(orig_post)
-                    return None
-                return Operator(char)
-            case "<":
-                stream.consume()
-                next_char = stream.get_char()
-                if next_char is None:
-                    return Operator(char)
-                if next_char == "=":
-                    stream.colcar_posicion(orig_post)
-                    return Operator(char + next_char)
-                if next_char == " " or is_digit(next_char):
-                    stream.colcar_posicion(orig_post)
-                    return Operator(char)
-                return Operator(char)
-            case ">":
-                stream.consume()
-                next_char = stream.get_char()
-                if next_char is None:
-                    return Operator(char)
-                if next_char == "=":
-                    return Operator(char + next_char)
-                if next_char == " " or is_digit(next_char):
-                    return Operator(char)
-                return Operator(char)
-            case "=":
-                stream.consume()
-                next_char = stream.get_char()
-                if next_char is None:
-                    return Operator(char)
-                if next_char == "=":
-                    return Operator(char + next_char)
-                if next_char == " " or is_digit(next_char):
-                    stream.colcar_posicion(orig_post)
-                    return None
-                return Operator(char)
-            case _:
-                return Operator(char)
-        return Operator(char)
-    else:
-        return None
+"""l = Stream("     <= 5")
+l.colcar_posicion(5)
+print(l.value, lexer_operator(l))
+print("posición final:", l.get_posicion())"""
 
-
-"""operadores=["+", "-", "*", "/", "<", ">", "<=", ">=", "==", "&", "|","~"]
-for op in operadores:
+"""operador=["+", "- ", "*", "/", "<", ">", "<=", ">=", "==", "&", "|","~"]
+for op in operador:
     print(op," es operador: ",lexer_operator(Stream(op)))"""
 
-
-def lexer_bool(stream: Stream) -> Optional[Bool]:
-    acc: list[str] = []
-    orig_post = stream.get_posicion()
-    char = stream.get_char()
-    if char is None:
-        return None
-    else:
-        if (char == "T") or (char == "F"):
-            acc.append(char)
-            stream.consume()
-            char = stream.get_char()
-            if char is None:
-                stream.colcar_posicion(orig_post)
-                return None
-            while char >= "a" and char <= "z":
-                acc.append(char)
-                stream.consume()
-                char = stream.get_char()
-                if char is None:
-                    break
-            final_str = "".join(acc)
-
-            if (
-                final_str == "True"
-            ):  # identifica que la cadena corresponda a la variable Booleana True o False
-                return Bool(True)
-            elif final_str == "False":
-                return Bool(False)
-
-            return None
-        else:
-            return None
+"""l = Stream("asaf <= 5")
+print(l.value, lexer_operator(l))
+print("posición final:", l.get_posicion())"""
 
 
-def lexer_unit(stream: Stream) -> Optional[UnitExp]:
-    acc: list[str] = []
-    orig_post = stream.get_posicion()
-    char = stream.get_char()
-    if char is None:
-        return None
-    else:
-        if (char == "u") or (char == "U"):
-            acc.append(char)
-            stream.consume()
-            char = stream.get_char()
-            if char is None:
-                stream.colcar_posicion(orig_post)
-                return None
-            while char >= "a" and char <= "z":
-                acc.append(char)
-                stream.consume()
-                char = stream.get_char()
-                if char is None:
-                    break
-            final_str = "".join(acc)
+def lexer_bool(
+    stream: Stream,
+) -> Optional[Bool]:  # buscador de variables booleanas
+    lexerTrue = reader_lexer("True", Bool(True))
+    evaluation = lexerTrue(stream)
+    if evaluation is None:
+        lexerFalse = reader_lexer("False", Bool(False))
+        evaluation = lexerFalse(stream)
+        return evaluation
+    return evaluation
 
-            if (
-                final_str == "unit"
-            ) or final_str == "Unit":  # identifica que la cadena corresponda a la expresion unit
-                return UnitExp()
-            
-            return None
-        else:
-            return None
+    # boolean_symbols=["True","False"]
+    # for b in boolean_symbols:
+    #    #print("****************************************************************")
+    #    lexer = reader_lexer(b, Bool(bool(b)))
+    #    #print("Se ejecutó identificador de lexer.")
+    #    evaluation = lexer(stream)
+
+    #    #comparation = (lexer(stream)  == Bool(bool(b)))
+    #    #print("Se ejecutó lexer. Comparado con", Bool(bool(b)), "es:",comparation)
+    #    #if comparation:
+    #    #    return Bool(bool(b))
+
+
+"""l = Stream("False<= 5")
+print("Posición de origen:", l.get_posicion())
+print("Se encontró", lexer_bool(l),"en", l.value)
+print("posición final:", l.get_posicion())"""
+
+
+def lexer_unit(# buscador de expresiones unit
+    stream: Stream,
+) -> Optional[UnitExp]:  
+    lexer = reader_lexer("unit", UnitExp())
+    return lexer(stream)
+
+
+"""l = Stream("unit<= 5")
+print("Posición de origen:", l.get_posicion())
+print("Se encontró", lexer_unit(l),"en", l.value)
+print("posición final:", l.get_posicion())"""
+
+
+def lexer_leftP(# buscador de apertura de paréntesis
+    stream: Stream,
+) -> Optional[LeftP]:  
+    lexer = reader_lexer("(", LeftP())
+    return lexer(stream)
+
+
+def lexer_rightP(# buscador de cierre de paréntesis
+    stream: Stream,
+) -> Optional[RightP]:  
+    lexer = reader_lexer(")", RightP())
+    return lexer(stream)
+
+
+def lexer_arrowR(# buscador de flechas
+    stream: Stream,
+) -> Optional[ArrowR]:  
+    lexer = reader_lexer("->", ArrowR())
+    return lexer(stream)
+
+
+def lexer_lineLambda(# buscador de expresiones lambda
+    stream: Stream,
+) -> Optional[LineLambda]:  
+    lexer = reader_lexer("->", LineLambda())
+    return lexer(stream)
+
+def lexer_twop(# buscador de expresiones lambda
+    stream: Stream,
+) -> Optional[Twop]:  
+    lexer = reader_lexer("->", Twop())
+    return lexer(stream)
+
+def lexer_if(# buscador de if
+    stream: Stream,
+) -> Optional[If]:  
+    lexer = reader_lexer("if", If())
+    return lexer(stream)
+
+def lexer_then(# buscador de Then
+    stream: Stream,
+) -> Optional[Then]:  
+    lexer = reader_lexer("then", Then())
+    return lexer(stream)
+
+def lexer_equals(# buscador de =
+    stream: Stream,
+) -> Optional[Equals]:  
+    lexer = reader_lexer("=", Equals())
+    return lexer(stream)
--- tests/test_prueba.py
+++ tests/test_prueba.py
@@ -4,6 +4,8 @@ from STLC_proj.lexer import (
     lexer_operator,
     lexer_bool,
     lexer_unit,
+    id_lexer,
+    reader_lexer,
     Stream,
     Variable,
     Int,
@@ -19,8 +21,8 @@ def make_positive_test(
     lexer, string: str, value
 ):  # Refactoriza los test predefinidos
     result = lexer(Stream(string))
-    # print(result)
-    # print(value)
+    print(result)
+    print(value)
     assert result == value
 
 
@@ -34,7 +36,7 @@ def make_negative_test(
 
 
 def make_positive_position_test(
-    lexer, string: str,final_position
+    lexer, string: str, final_position
 ):  # Refactoriza los test predefinidos para la posición
     s = Stream(string)
     lex_var = lexer(s)
@@ -51,8 +53,26 @@ def make_negative_position_test(
     assert s.get_posicion() == 0  # verifica regreso a posición original
 
 
+def make_positive_id_test(
+    string: str, stream: str, value
+):  # Refactoriza los test predefinidos para la identificación de cadenas
+    found_lexer = id_lexer(string)
+    result = found_lexer(Stream(stream))
+    #print(found_lexer)
+    #print(result)
+    assert result == value
+
+
+def make_negative_id_test(
+    string: str, stream:str
+):  # Refactoriza los test predefinidos para la identificación de cadenas
+    found_lexer = id_lexer(string)
+    result = found_lexer(Stream(stream))
+    assert result is None
+
 
 """ ***************** Test de Variable  ******************** """
+
 
 @pytest.mark.parametrize(
     "stream,expected",
@@ -61,21 +81,22 @@ def make_negative_position_test(
         ("cajas = 5", Variable("cajas")),
         ("caJas = 5", Variable("caJas")),  # prueba con mayúsculas
         ("c = 5", Variable("c")),  # prueba con una sola variable
-    ]
+    ],
 )
 def test_variable_positive(stream: str, expected: Variable):
     make_positive_test(lexer_variable, stream, expected)
 
-@pytest.mark.parametrize( # Prueba cadenas con salida None
+
+@pytest.mark.parametrize(  # Prueba cadenas con salida None
     "stream",
-    [  
+    [
         "_",  # Prueba con guión
         "125caJas = 5",  # Prueba con dígitos
         "=$125caJas = 5",  # Prueba con simbolos no reconocidos
         "True",  # Prueba con booleanas
         "False",
-        ""  # Prueba con cadena vacía
-    ]
+        "",  # Prueba con cadena vacía
+    ],
 )
 def test_variable_negative(stream: str):
     make_negative_test(lexer_variable, stream)
@@ -90,8 +111,7 @@ def test_lexer_variable_positive_posicion(stream: str):
 
 
 @pytest.mark.parametrize(
-    "stream",
-    ["_", "125caJas = 5", "=$125caJas = 5", "True", "False", ""]
+    "stream", ["_", "125caJas = 5", "=$125caJas = 5", "True", "False", ""]
 )  # Prueba posicion 0 para cadenas con salida None
 def test_lexer_variable_negative_posicion(stream: str):
     make_negative_position_test(lexer_variable, stream)
@@ -99,23 +119,24 @@ def test_lexer_variable_negative_posicion(stream: str):
 
 """ ***************** Test de Int  ******************** """
 
-@pytest.mark.parametrize( # Prueba enteros positivos y negativos
+
+@pytest.mark.parametrize(  # Prueba enteros positivos y negativos
     "stream,expected",
     [
-        ("2", Int(2)), ("-3", Int(-3)), ("5569", Int(5569)), ("-869", Int(-869))  # Prueba variables con guión
-        
-    ]
+        ("2", Int(2)),
+        ("-3", Int(-3)),
+        ("5569", Int(5569)),
+        ("-869", Int(-869)),  # Prueba variables con guión
+    ],
 )
-def test_int_positive(stream: str, expected: Variable):
+def test_lexer_int_positive(stream: str, expected: Variable):
     make_positive_test(lexer_int, stream, expected)
 
-@pytest.mark.parametrize( # Prueba cadenas con salida None
-    "stream",
-    [  
-        "xtrs=56", "+xtrs=56", "", "_", "_4556", "$#"
-    ]
+
+@pytest.mark.parametrize(  # Prueba cadenas con salida None
+    "stream", ["xtrs=56", "+xtrs=56", "", "_", "_4556", "$#"]
 )
-def test_int_negative(stream: str):
+def test_lexer_int_negative(stream: str):
     make_negative_test(lexer_int, stream)
 
 
@@ -133,80 +154,110 @@ def test_lexer_int_positive_posicion(stream: str):
 )  # Prueba posicion 0 para cadenas con salida None
 def test_lexer_int_negative_posicion(stream: str):
     make_negative_position_test(lexer_int, stream)
-    
 
 
 """ ***************** Test de Operador  ******************** """
+"""
 
-@pytest.mark.parametrize( # Prueba enteros positivos y negativos
+@pytest.mark.parametrize(  # Prueba operadores predefinidos
     "stream,expected",
     [
-        ("2", Int(2)), ("-3", Int(-3)), ("5569", Int(5569)), ("-869", Int(-869))  # Prueba variables con guión
-        
-    ]
+        ("+", Operator("+")),
+        ("- ", Operator("- ")),
+        ("*", Operator("*")),
+        ("/", Operator("/")),
+        ("<", Operator("<")),
+        (">", Operator(">")),
+        ("<=", Operator("<=")),
+        (">=", Operator(">=")),
+        ("==", Operator("==")),
+        ("&", Operator("&")),
+        ("|", Operator("|")),
+        ("~", Operator("~")), 
+        ("- 589", Operator("- ")),
+    ],
 )
-def test_int_positive(stream: str, expected: Variable):
-    make_positive_test(lexer_int, stream, expected)
+def test_lexer_operator_positive(stream: str, expected: Variable):
+    make_positive_test(lexer_operator, stream, expected)
 
-@pytest.mark.parametrize( # Prueba cadenas con salida None
-    "stream",
-    [  
-        "xtrs=56", "+xtrs=56", "", "_", "_4556", "$#"
-    ]
+
+@pytest.mark.parametrize(  # Prueba cadenas con salida None
+    "stream", ["", "-1", "", "_", "asde + 5", "$#", "= "]
 )
-def test_int_negative(stream: str):
-    make_negative_test(lexer_int, stream)
+def test_lexer_operator_negative(stream: str):
+    make_negative_test(lexer_operator, stream)
 
 
 @pytest.mark.parametrize(
-    "stream", ["5", "589", "-8546s"]
-)  # Prueba posicion para cadenas con salida Variable
-def test_lexer_int_positive_posicion(stream: str):
-    lenght = len(str(lexer_int(Stream(stream)).value))
-    make_positive_position_test(lexer_int, stream, lenght)
+    "stream",
+    ["+", "- ", "*", "/", "<", ">", "<=", ">=", "==", "&", "|", "~", "- 8546s"],
+)  # Prueba posicion para cadenas con salida Operador
+def test_lexer_operator_positive_posicion(stream: str):
+    lenght = len(str(lexer_operator(Stream(stream)).name))
+    make_positive_position_test(lexer_operator, stream, lenght)
 
 
 @pytest.mark.parametrize(
     "stream",
-    ["_", "caJas", "=$125caJas = 5", "True", "False"],
+    ["", "_", "caJas", "=$125caJas = 5", "True", "False", "-8546s"],
 )  # Prueba posicion 0 para cadenas con salida None
-def test_lexer_int_negative_posicion(stream: str):
-    make_negative_position_test(lexer_int, stream)
+def test_lexer_operator_negative_posicion(stream: str):
+    make_negative_position_test(lexer_operator, stream)
+"""
+
+""" *****************Test identificador de cadenas ***************************** """
 
 
-
-def test_lexer_operador_vacio():
-    make_negative_test(lexer_operator, "")
-
-
-def test_lexer_operator_is_operador():
-    operadores = ["+", "-", "*", "/", "<", ">", "<=", ">=", "==", "&", "|", "~"]
-    for op in operadores:
-        make_positive_test(lexer_operator, op, Operator(op))
-
-
-def test_lexer_operador_negativo():
-    make_negative_test(lexer_operator, "-1")
-
-
-def test_lexer_operador_espacio():
-    make_positive_test(lexer_operator, "- 589", Operator("-"))
-
-
-def test_lexer_operador_caracteres():
-    make_negative_test(lexer_operator, "asde + 5")
-
-
-def test_lexer_operador_otrosvalores():
-    make_negative_test(lexer_operator, "$'?")
+"""@pytest.mark.parametrize(  # Prueba operadores aislados
+    "stream,expected",
+    [
+        ("+", Operator("+")),
+        ("- ", Operator("- ")),
+        ("*", Operator("*")),
+        ("/", Operator("/")),
+        ("<", Operator("<")),
+        (">", Operator(">")),
+        ("<=", Operator("<=")),
+        (">=", Operator(">=")),
+        ("==", Operator("==")),
+        ("&", Operator("&")),
+        ("|", Operator("|")),
+        ("~", Operator("~")),  # Prueba operadores aislados
+        ("- 589", Operator("- ")),
+    ],
+)
+def test_lexer_operator_positive(stream: str, expected: Variable):
+    make_positive_id_test(lexer_operator,stream, expected)"""
 
 
-def test_lexer_operador_igual():
-    make_negative_test(lexer_operator, "= ")
+@pytest.mark.parametrize(  # Prueba identificación de bool
+    "stream,symbol,expected",
+    [
+        ("True", "True", "True"),
+        ("False ", "False", "False"),
+        ("True45s45a", "True", "True"),
+        ("False+-g ", "False", "False"),
+    ],
+)
+def test_lexer_bool_positive(stream: str, symbol: str, expected: str):
+    make_positive_id_test(symbol, stream, expected)
 
+@pytest.mark.parametrize(  # Prueba identificación de bool con salida None
+    "stream,symbol",
+    [
+        ("False ", "True"),
+        ("True ", "False"),
+        ("Tre45s45a", "True"),
+        (" False+-g ", "False"),
+        (" ", "False"),
+        ("58False", "False"),
+    ],
+)
+def test_lexer_bool_negative(stream: str, symbol: str):
+    make_negative_id_test(symbol, stream)
 
-""" ***************** Test de Bool  ******************** """
-
+""" ***************** Test de Bool, Unit, Parentesis, puntos, flecha, if..then   ******************** """
+"""
 
 def test_lexer_bool_True():
     make_positive_test(lexer_bool, "True", Bool(True))
@@ -234,6 +285,6 @@ def test_lexer_bool_parentesis():
 
 def test_lexer_bool_otros_simbolos():
     make_negative_test(lexer_bool, "#$(True)")
-
+"""
 
 # Faltan test de posición al fallo NO VARIABLE
